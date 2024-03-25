9b782cc17b338b78fbdb028241144d89eecd12eb
Antwon W <67429225+httpantwon@users.noreply.github.com>
2024-03-25 18:06:00 -0500

    Add files via upload

---
+++ ios101-project5-tumblr-starter-main/ios101-project5-tumblr/BlogCell.swift
@@ -0,0 +1,26 @@
+//
+//  BlogCell.swift
+//  ios101-project5-tumblr
+//
+//  Created by Antwon Walls on 3/24/24.
+//
+
+import UIKit
+
+class BlogCell: UITableViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+       
+        // Configure the view for the selected state
+    }
+}
